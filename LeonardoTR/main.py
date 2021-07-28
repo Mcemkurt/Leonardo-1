18d138d1d2be8953baee612805224826b317e021
Mcemkurt <80649033+Mcemkurt@users.noreply.github.com>
2021-07-28 16:33:41 +0300

    Add files via upload

---
+++ LeonardoTR/main.py
@@ -0,0 +1,11 @@
+from PyQt5.QtWidgets import *
+from gui_edit1_python import Ui_MainWindow
+
+if __name__ == "__main__":
+    import sys
+    app = QApplication(sys.argv)
+    MainWindow = QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
